ab02a99eba25f540af0435b8dccd39e065555248
Alon Elmaliah <alon.elmaliah@gmail.com>
2018-04-04 17:20:47 +0300

    run all accepts PR #

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+if [ ! -d "iri" ]; then
+  echo "please first clone IRI, exiting..."
+  exit -1
+fi
+cd iri
+git fetch origin pull/$1/head
+git checkout -b pr/$1 FETCH_HEAD
+
+mvn package
+
+cd ..
+
 bash 1_start_and_stop_a_node_without_database-mainnet.sh
 bash 1_start_and_stop_a_node_without_database-testnet.sh
 bash 2_start_and_stop_a_node_known_database-mainnet.sh
