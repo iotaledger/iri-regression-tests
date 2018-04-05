d397d9b8c9e667b0b47180b27b1ac97166f1a545
galrogo <gal.rogozinski@iota.org>
2018-04-05 15:42:45 +0300

    create transaction on node_a find it on node_b

--- start_and_stop_a_node.sh
+++ start_and_stop_a_node.sh
@@ -3,50 +3,64 @@
 #$2 - port
 #$3 - is testnet?
 #$4 - unpack DB?
-#$5 - python script
+#$5 - number of nodes to load
+#$6 - python script
+
+echo "starting node"
+port=$2
 cd iri/
-rm -rf node
-cp -rf target node
+for (( i=1; i<=$5; i++))
+do
+    node='node'$i
+    echo $node
+    rm -rf $node
+    cp -rf target $node
 
-cd node
-#TODO host mainnet/testnet DB somewhere
-if $4;
-then
-    if $3;
+    cd $node
+    #TODO host mainnet/testnet DB somewhere
+    if $4;
     then
-    echo "unpack testnet DB"
-    tar -xzf ../../testnetdb.tar.gz;
-    else
-    echo "unpack mainnet DB"
-    tar -xzf ../../mainnetdb.tar.gz;
+        if $3;
+        then
+        echo "unpack testnet DB"
+        tar -xzf ../../testnetdb.tar.gz;
+        else
+        echo "unpack mainnet DB"
+        tar -xzf ../../mainnetdb.tar.gz;
+        fi
     fi
-fi
 
-#TODO read version from config
-if $3;
-then
-echo "start node.. testnet"
-java -jar iri-$1.jar -p $2 -u $2 --testnet &> iri.log &
-else
-echo "start node.. mainnet"
-java -jar iri-$1.jar -p $2 -u $2 &> iri.log &
-fi
-echo $! > iri.pid
-cd ..
+    #TODO read version from config
+    if $3
+    then
+    echo "start node.. testnet on port: "$port
+    java -jar iri-$1.jar -p $port -u $port --testnet &> iri.log &
+    else
+    echo "start node.. mainnet on port: "$port
+    java -jar iri-$1.jar -p $port -u $port &> iri.log &
+    fi
+    echo $! > iri.pid
+    cd ..
+    ((port++))
+done
 
 #give time to the node to init
 #TODO instead of sleep sample API until it is up
 sleep 40
-if [ -n "$5" ];
+if [ -n "$6" ];
 then
 echo "start python script.."
-python $5 $2
+python $6 $2
 fi
 
 #stop node
 echo "stop node.."
-kill `cat node/iri.pid`
-wait `cat node/iri.pid`
+for (( i=1; i<=$5; i++))
+do
+    node='node'$i
+    kill `cat $node/iri.pid`
+    wait `cat $node/iri.pid`
+done
 
 #Check log for errors
 grep -i "error" node/iri.log
