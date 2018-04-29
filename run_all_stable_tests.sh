46a429b10f8f02d35fe98dcab0133c5e5bfe550f
Alon Elmaliah <alon.elmaliah@gmail.com>
2018-04-29 20:28:12 +0300

    add abitity to call test with version number

--- run_all_stable_tests.sh
+++ run_all_stable_tests.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-bash 1_start_and_stop_a_node_without_database-mainnet.sh
+
+#default version
+if [ -z "$1" ];
+then VER="1.4.2.3";
+else VER=$1;
+fi
+
+bash 1_start_and_stop_a_node_without_database-mainnet.sh $1
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 1_start_and_stop_a_node_without_database-testnet.sh
+bash 1_start_and_stop_a_node_without_database-testnet.sh $1
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh
+bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh $1
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
