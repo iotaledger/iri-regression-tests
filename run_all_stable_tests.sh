d02455822c9c4d0f46fad7ac86f036547b5ee42c
galrogo <gal.rogozinski@iota.org>
2018-05-02 16:34:54 +0300

    fix version parameter

--- run_all_stable_tests.sh
+++ run_all_stable_tests.sh
@@ -6,9 +6,9 @@ then VER="1.4.2.4";
 else VER=$1;
 fi
 
-bash 1_start_and_stop_a_node_without_database-mainnet.sh $1
+bash 1_start_and_stop_a_node_without_database-mainnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 1_start_and_stop_a_node_without_database-testnet.sh $1
+bash 1_start_and_stop_a_node_without_database-testnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh $1
+bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
