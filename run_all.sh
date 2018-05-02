6eaa375944fb130f1a03f1df1a95639d661fbf8b
galrogo <gal.rogozinski@iota.org>
2018-05-02 16:56:51 +0300

    when running all tests, force inputing version

--- run_all.sh
+++ run_all.sh
@@ -7,11 +7,19 @@ cd iri
 git fetch origin pull/$1/head
 git checkout -b pr/$1 FETCH_HEAD
 
+if [ -z "$2" ]
+then
+    echo version is missing!
+    exit -1
+else
+    VER=$2
+fi
+
 mvn package
 
 cd ..
 
-bash 1_start_and_stop_a_node_without_database-mainnet.sh
-bash 1_start_and_stop_a_node_without_database-testnet.sh
-bash 2_start_and_stop_a_node_known_database-mainnet.sh
-bash 2_start_and_stop_a_node_known_database-testnet.sh
+bash 1_start_and_stop_a_node_without_database-mainnet.sh $VER
+bash 1_start_and_stop_a_node_without_database-testnet.sh $VER
+bash 2_start_and_stop_a_node_known_database-mainnet.sh $VER
+bash 2_start_and_stop_a_node_known_database-testnet.sh $VER
