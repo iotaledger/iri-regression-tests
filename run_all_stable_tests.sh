e9aee546195fa9c0d72fbd3badc75981e20c90e6
galrogo <gal.rogozinski@iota.org>
2018-05-07 16:41:33 +0300

    tests work. Now running 2_start_and_stop_a_node_known_database-testnet

--- run_all_stable_tests.sh
+++ run_all_stable_tests.sh
@@ -7,16 +7,12 @@ else VER=$1;
 fi
 
 echo "running tests"
-echo "1_start_and_stop_a_node_without_database-mainnet.sh" 
 bash 1_start_and_stop_a_node_without_database-mainnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-echo "1_start_and_stop_a_node_without_database-testnet.sh"
 bash 1_start_and_stop_a_node_without_database-testnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 2_start_and_stop_a_node_known_database-testnet.sh
-echo "bash 2_start_and_stop_a_node_known_database-testnet.sh"
+bash 2_start_and_stop_a_node_known_database-testnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh $VER
-echo "bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh" 
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-echo "finished running tests"
+echo "finished running tests successfully"
