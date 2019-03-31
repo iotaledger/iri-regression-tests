7c74192d0d4de1ec4ca6aba29e1d679e77a62e1d
DyrellC <dyrell.chapman@iota.org>
2019-03-31 01:56:24 -0600

    Remove unnecessary logs

--- Nightly-Tests/Sync-Tests/createCluster.sh
+++ Nightly-Tests/Sync-Tests/createCluster.sh
@@ -21,8 +21,6 @@ cd $base_dir
 
 UUID="$(uuidgen)"
 K8S_NAMESPACE=$(kubectl config get-contexts $(kubectl config current-context) | tail -n+2 | awk '{print $5}')
-echo $UUID
-echo $K8S_NAMESPACE
 
 git clone https://github.com/iotaledger/tiab
 
@@ -35,13 +33,11 @@ cd ../
 echo "Starting TIAB"
 python tiab/create_cluster.py -i iotacafe/iri-dev:latest -t ${UUID} -n ${K8S_NAMESPACE} -c ./config.yml -o ./output.yml -d
 echo "Cluster created"
-echo "Running ZMQ Tests"
 
+echo "Running ZMQ Tests"
 python zmqTest.py -o ./SyncOutput
 
 echo "Tearing down cluster"
-echo $UUID
-echo $K8S_NAMESPACE
 python tiab/teardown_cluster.py -t $UUID -n $K8S_NAMESPACE
 
 echo "Deactivating"
