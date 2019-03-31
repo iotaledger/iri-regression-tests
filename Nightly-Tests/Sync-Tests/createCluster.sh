4dc03a22b4cf133abe275d09d9095b40e1d3cdba
DyrellC <dyrell.chapman@iota.org>
2019-03-31 01:49:15 -0600

    Fix setup

--- Nightly-Tests/Sync-Tests/createCluster.sh
+++ Nightly-Tests/Sync-Tests/createCluster.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+echo "Downloading apt requirments "
+sed 's/#.*//' requirements.txt | xargs sudo apt-get install -y
+
+echo "Starting Venv"
+python3 -m venv ./venv
+base_dir=$(pwd)
+
+cd ./venv/bin/
+python_bin=$(pwd)
+
+source ./activate
+cd $base_dir
+cd ../
+
+echo "Installing python requirements"
+pip install --upgrade pip
+pip install -e .
+cd $base_dir
+
 
 UUID="$(uuidgen)"
 K8S_NAMESPACE=$(kubectl config get-contexts $(kubectl config current-context) | tail -n+2 | awk '{print $5}')
@@ -7,19 +26,18 @@ echo $K8S_NAMESPACE
 
 git clone https://github.com/iotaledger/tiab
 
-#cd tiab
-#virtualenv venv
-#source ./venv/bin/activate
-#pip install -r requirements.txt
-#cd ../
-pip install pyzmq --user
+cd tiab
+virtualenv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+cd ../
+
 echo "Starting TIAB"
-ls tiab
 python tiab/create_cluster.py -i iotacafe/iri-dev:latest -t ${UUID} -n ${K8S_NAMESPACE} -c ./config.yml -o ./output.yml -d
 echo "Cluster created"
 echo "Running ZMQ Tests"
 
-python zmqTest.py
+python zmqTest.py -o ./SyncOutput
 
 echo "Tearing down cluster"
 echo $UUID
--- Nightly-Tests/setup.py
+++ Nightly-Tests/setup.py
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         "PyOTA == 2.0.7",
         "psutil == 5.6.1",
-        "matplotlib == 2.2.4"
+        "matplotlib == 2.2.4",
+        "pyzmq == 18.0.1"
     ],
 )
