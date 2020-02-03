38e0f28bbc89701756f464509d187fae0fe3887f
Andrea Villa <andreakarimodm@gmail.com>
2020-02-03 16:50:18 +0100

    Provide SYNC_IMAGE envvar to test arbitrary images, defaults to iotacafe/iri-dev

--- Nightly-Tests/Sync-Tests/createCluster.sh
+++ Nightly-Tests/Sync-Tests/createCluster.sh
@@ -27,7 +27,7 @@ sed 's/#.*//' requirements.txt | xargs apt-get install -y
 UUID="$(uuidgen)"
 K8S_NAMESPACE=$(kubectl config get-contexts $(kubectl config current-context) | tail -n+2 | awk '{print $5}')
 base_dir=$(pwd)
-IMAGE=$1
+IMAGE=${SYNC_IMAGE:-"iotacafe/iri-dev"}
 DATE=$(date '+%Y-%m-%d')
 
 if [ ! -d tiab ]; then
